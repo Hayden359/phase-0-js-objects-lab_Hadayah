2158988f832d8cc1536cf3c4873725d9c9902809
Hayden359 <hcwrite@outlook.com>
2026-09-10 15:40:34 -0500

    assignment submission

--- index.js
+++ index.js
@@ -1,4 +1,55 @@
-//Write your code here
+//Objects
+  let attendee = 
+    ["attendeeID" , "name" , "event" , "ticketType" , "ticketPrice"]
+  
+  //functions
+
+  function logAttendeeName(name) {
+  attendee.name = name;
+  console.log(attendee.name);
+  return attendee.name;
+  }
+
+  function logTicketPrice(price) {
+  attendee.ticketPrice = price;
+  console.log(attendee.ticketPrice);
+   return attendee.ticketPrice;
+  }
+
+  function updateTicketType(ticketType) {
+  attendee.ticketType = ticketType;
+  console.log(attendee.ticketType);
+    return attendee.ticketType;
+  }
+
+  function updateTicketPrice(ticketPrice) {
+  attendee.ticketPrice = ticketPrice;
+  console.log(attendee.ticketPrice);
+    return attendee.ticketPrice;
+  }
+
+  function removeEventProperty() {
+  delete attendee.event;
+  console.log(attendee.event);
+    return attendee.event;
+  }
+
+  function addCheckedInProperty() {
+  attendee.checkedIn = true;
+  console.log(attendee);
+    return attendee;
+  }
+
+  //activate functions
+   attendee.attendeeID = "T001";
+ logAttendeeName("Alice Smith");
+
+ logTicketPrice(150.00);
+
+ updateTicketType("VIP");
+
+ 
+
 
 
 
